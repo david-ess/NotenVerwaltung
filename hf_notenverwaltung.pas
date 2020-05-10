472e96d7e08b2420085e20268aa7b72ec98e8a52
David Hagedorn <hage89@gmx.de>
2020-05-11 01:06:02 +0200

    GUI erstellt

--- hf_notenverwaltung.pas
+++ hf_notenverwaltung.pas
@@ -5,10 +5,20 @@ unit HF_Notenverwaltung;
 interface
 
 uses
-  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls,
+  Spin;
 
 type
+
+  { TForm1 }
+
   TForm1 = class(TForm)
+    Button1: TButton;
+    Button2: TButton;
+    ComboBox1: TComboBox;
+    Label1: TLabel;
+    ListBox1: TListBox;
+    SpinEdit1: TSpinEdit;
   private
     { private declarations }
   public
